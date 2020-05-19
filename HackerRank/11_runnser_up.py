ed9e86e101d9dc2fb3ba2442de09a6c0b05a7d38
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-19 20:20:52 +0600

    Update 11_runnser_up.py

--- HackerRank/11_runnser_up.py
+++ HackerRank/11_runnser_up.py
@@ -1,11 +1,8 @@
-n = int(input())
-
 nums = map(int, input().split())
 print(sorted(list(set(nums)))[-2])
 
 
 print("Optional Solution")
-n=int(input())
 arr = map(int, input().split())
 newlist = []
 for i in arr:

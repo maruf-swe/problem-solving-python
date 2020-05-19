b6e002a85047a2df90a5e8b0b40cbe5c4c25ec74
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-20 05:28:44 +0600

    Update no_idea14.py

--- HackerRank/no_idea14.py
+++ HackerRank/no_idea14.py
@@ -1,16 +1,18 @@
-n,m=map(int,input().split())
-l=input().split(' ')
-A=set(input().split(' '))
-B=set(input().split(' '))
-happiness=0
+n,m = map(int,input().split())
+l = input().split(' ')
+A = set(input().split(' '))
+B = set(input().split(' '))
+happiness = 0
 
 for i in l:
     if i in A:
-        happiness+=1
+        happiness += 1
     if i in B:
-        happiness-=1
+        happiness -= 1
 print(happiness)
 
+
+another solutin
 n, m = map(int, input().split())
 array = list(map(int, input().split()))
 a = set(map(int, input().split()))

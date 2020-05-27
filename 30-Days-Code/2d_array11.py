667a74cc42d8b71663c40ef23d20e9570bda8392
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-28 02:10:18 +0600

    11 days

---
+++ 30-Days-Code/2d_array11.py
@@ -0,0 +1,13 @@
+my_hourglasses = list()
+
+for i in range(0,4):
+    for j in range(0,4):
+        hourglass = list()
+        hourglass += arr[i][j:j+3]
+        hourglass.append(arr[i+1][j+1])
+        hourglass += arr[i+2][j:j+3]
+        my_hourglasses.append(hourglass)
+
+hourglasses_sums = [sum(item) for item in my_hourglasses]
+
+print(max(hourglasses_sums))

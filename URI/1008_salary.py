65152b6105b2ee444afe8267bda5bc22e846c12f
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-29 00:37:57 +0600

    salary

---
+++ URI/1008_salary.py
@@ -0,0 +1,5 @@
+emplyoes_number = int(input())
+working_hour = int(input())
+per_worked_salary = float(input())
+print("NUMBER =", emplyoes_number)
+print("SALARY = U$ %0.2f" % (working_hour * per_worked_salary))

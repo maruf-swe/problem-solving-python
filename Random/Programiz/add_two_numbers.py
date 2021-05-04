bd73db32a098de866f9ab85de204030c7e0b5f12
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-05-04 22:20:06 +0600

    first commit

---
+++ Random/Programiz/add_two_numbers.py
@@ -0,0 +1,11 @@
+# how to add two numbers
+a = 5
+b = 5
+c = a + b  # or  print(a + b)
+print(c)
+
+# how to add by user input
+
+a = input("Please insert an value A: ")
+b = input("Please insert an value B: ")
+print(a + b)

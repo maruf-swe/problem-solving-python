9cab114ce6c87831b51dfe331571d489278190e4
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-05-04 23:50:18 +0600

    triangle area

---
+++ Random/Programiz/triangle_area.py
@@ -0,0 +1,11 @@
+# triangle has three sides,,, a, b , c
+# find the triangle Area
+a = 18
+b = 24
+c = 30
+
+# perimeter
+p = (a + b + c) / 2
+
+area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+print("The perimeter Area is", + area)

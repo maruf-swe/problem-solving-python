239fed1a508ba39a4a4555916ade37b186d84ff0
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-05-08 23:55:56 +0600

    Add files via upload

---
+++ Random/Programiz/positive_negetiv_zero.py
@@ -0,0 +1,19 @@
+# Using if...elif...else
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+# using nested if else
+num = float(input("Enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive number")
+else:
+    print("Negative number")

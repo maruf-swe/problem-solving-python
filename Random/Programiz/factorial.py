b0f65f48fcc94d1223124c3472f545dcebaf1037
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-06-17 00:00:00 +0600

    Update factorial.py

--- Random/Programiz/factorial.py
+++ Random/Programiz/factorial.py
@@ -12,3 +12,20 @@ else:
     for i in range(1, num + 1):
         factorial = factorial * i
     print("The factorial of", num, "is", factorial)
+    
+    
+# another solution
+
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+
+    return n * factorial(n - 1)
+
+
+num = 5
+print("Factorial of", num, "is",factorial(num))
+
+

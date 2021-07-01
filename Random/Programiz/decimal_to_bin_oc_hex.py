9854f828ce191a040e60724ff6d3b47914ab872d
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-01 21:21:05 +0600

    Add files via upload

---
+++ Random/Programiz/decimal_to_bin_oc_hex.py
@@ -0,0 +1,7 @@
+# with built in function
+dec = int(input())
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")

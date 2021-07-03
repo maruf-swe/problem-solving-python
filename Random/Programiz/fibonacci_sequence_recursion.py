e13061c2e4ebe825c4006c222e833325cd667302
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-03 19:46:18 +0600

    Add files via upload

---
+++ Random/Programiz/fibonacci_sequence_recursion.py
@@ -0,0 +1,17 @@
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return (recur_fibo(n - 1) + recur_fibo(n - 2))
+
+
+terms = int(input())
+
+if terms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(terms):
+        print(recur_fibo(i))

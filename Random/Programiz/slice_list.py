d3da1d031a1f928b3a96dc328c8a3fcd8c09f81c
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-10 08:17:59 +0600

    Add files via upload

---
+++ Random/Programiz/slice_list.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+# Get all the Items After a Specific Position
+print(my_list[2:])
+
+# Get all the Items Before a Specific Position
+print(my_list[:2])
+
+# Get all the Items from One Position to Another Position
+print(my_list[2:4])
+# Get the Items at Specified Intervals
+print(my_list[::2])

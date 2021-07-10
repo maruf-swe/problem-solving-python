8f4018414cd626128375e0cf2cd16329975249d1
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-10 08:28:18 +0600

    Add files via upload

---
+++ Random/Programiz/list_isempty.py
@@ -0,0 +1,12 @@
+# Python Program to Check If a List is Empty
+my_list = []
+if not my_list:
+    print("the list is empty")
+
+my_list = []
+if not len(my_list):
+    print("the list is empty")
+    
+my_list = []
+if my_list == []:
+    print("The list is empty")

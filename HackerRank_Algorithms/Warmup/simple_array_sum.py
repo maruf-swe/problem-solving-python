6ced4027cae757c10878ebad534a62ece57ee127
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2021-07-12 21:43:30 +0600

    old file upload

---
+++ HackerRank_Algorithms/Warmup/simple_array_sum.py
@@ -0,0 +1,21 @@
+def simpleArraySum(ar):
+    total = sum(ar)
+    return total
+
+ar_c = int(input())
+ar = list(map(int, input().rstrip().split()))
+print(simpleArraySum(ar))
+
+
+#another solution
+
+def simpleArraySum(ar):
+    total = 0
+    for i in range((len(ar))):
+        total += ar[i]
+    return total
+
+
+ar_c = int(input())
+ar = list(map(int, input().rstrip().split()))
+print(simpleArraySum(ar))

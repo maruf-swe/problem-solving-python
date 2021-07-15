a61b9e04efdc428f9a7412ca9d56788cd5553dfe
Maruf <marufur1847@gmail.com>
2021-07-16 05:04:56 +0600

    update 1015

--- URI/1015_distance_between_two_points.py
+++ URI/1015_distance_between_two_points.py
@@ -2,4 +2,13 @@ x1, y1 = list(map(float, input().split()))
 x2, y2 = list(map(float, input().split()))
 dis = (x2 - x1) ** 2 + (y2 - y1) ** 2
 distance = dis ** .5
-print("%.4f" %distance)
+print("%.4f" % distance)
+
+
+# another solution
+import math
+
+x1, y1 = list(map(float, input().split()))
+x2, y2 = list(map(float, input().split()))
+distance = math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+print("%0.4f" % distance)

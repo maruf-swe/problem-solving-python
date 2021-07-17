2796e252e0ac29373a1d21c9715fbd85e925e89a
Maruf <marufur1847@gmail.com>
2021-07-17 23:18:07 +0600

    first commit

--- URI/1019_time_conversation.py
+++ URI/1019_time_conversation.py
@@ -1,6 +1,26 @@
-n = int(input("input seconds"))
+# another
+n = int(input())
 print("{}:{}:{}".format(int(n / 3600), int(n % 3600 / 60), int(n % 60)))
+
+n = int(input("input seconds"))  # for user input msg doesn't accept
+print("{}:{}:{}".format(int(n / 3600), int(n % 3600 / 60), int(n % 60)))
+
 
 # another way
 import datetime
 print(str(datetime.timedelta(seconds=666)))
+
+# another
+seconds = int(input("input: "))
+minutes = int(seconds / 60)
+seconds %= 60
+hours = int(minutes / 60)
+print('{}:{}:{}'.format(hours, minutes, seconds))
+
+# another
+S = int(input())
+H = int(S / (60 * 60))
+S = S % (60 * 60)
+M = int(S / 60)
+S = S % 60
+print("%d:%d:%d" % (H, M, S))

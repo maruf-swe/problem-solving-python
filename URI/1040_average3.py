66984bb206cb924caddca5deb09105943081662f
Maruf <marufur1847@gmail.com>
2021-07-20 22:30:23 +0600

    variable update

--- URI/1040_average3.py
+++ URI/1040_average3.py
@@ -1,17 +1,17 @@
-n1, n2, n3, n4 = list(map(float, input().split()))
-a = (((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10)
-print("Media: %.1lf" % a)
-if (a >= 7.0):
+N1, N2, N3, N4 = list(map(float, input().split()))
+average = (((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10)
+print("Media: %.1lf" % average)
+if (average >= 7.0):
     print("Aluno aprovado.")
-elif (a < 5.0):
+elif (average < 5.0):
     print("Aluno reprovado.")
-elif (a >= 5.0 and a <= 6.9):
+elif (average >= 5.0 and average <= 6.9):
     print("Aluno em exame.")
-    n5 = float(input())
-    print("Nota do exame: %.1lf" % n5)
-    b = (a + n5) / 2
-    if (b >= 5.0):
+    N5 = float(input())
+    print("Nota do exame: %.1lf" % N5)
+    one_more = (average + N5) / 2
+    if (one_more >= 5.0):
         print("Aluno aprovado.")
-    if (b <= 4.9):
+    if (one_more <= 4.9):
         print("Aluno reprovado.")
-    print("Media final: %.1lf" % b)
+    print("Media final: %.1lf" % one_more)

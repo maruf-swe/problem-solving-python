b5cc14d7fe3baaad1647e100d76a592d7c8fe74a
Maruf <marufur1847@gmail.com>
2021-07-24 22:43:03 +0600

    game time with minutes update

--- URI/1047_game_time_minutes.py
+++ URI/1047_game_time_minutes.py
@@ -1,12 +1,14 @@
+# s_h = start hour, s_m = start minute, f_h = finish hour, f_m = finish minute
 s_h, s_m, f_h, f_m = list(map(int, input().split()))
-fh = f_h - s_h
-if (fh < 0):
-    fh = 24 + (f_h - s_h)
-fm = f_m - s_m
-if (fm < 0):
-    fm = 60 + (f_m - s_m)
-    fh = fh - 1
+total_hours = f_h - s_h
+if (total_hours < 0):
+    total_hours = 24 + (f_h - s_h)
+total_minutes = f_m - s_m
+if (total_minutes < 0):
+    total_minutes = 60 + (f_m - s_m)
+    total_hours = total_hours - 1
+
 if (f_h == s_h and f_m == s_m):
     print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
 else:
-    print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(fh, fm))
+    print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(total_hours, total_minutes))

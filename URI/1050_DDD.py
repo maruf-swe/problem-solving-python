c627335556d06eac4b28ad61b8239dca0163c276
Maruf <marufur1847@gmail.com>
2021-07-25 23:27:20 +0600

    add DDD

---
+++ URI/1050_DDD.py
@@ -0,0 +1,19 @@
+phone_dial = int(input())
+if (phone_dial == 61):
+    print("Brasilia")
+elif (phone_dial == 71):
+    print("Salvador")
+elif (phone_dial == 11):
+    print("Sao Paulo")
+elif (phone_dial == 21):
+    print("Rio de Janeiro")
+elif (phone_dial == 32):
+    print("Juiz de Fora")
+elif (phone_dial == 19):
+    print("Campinas")
+elif (phone_dial == 27):
+    print("Vitoria")
+elif (phone_dial == 31):
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")

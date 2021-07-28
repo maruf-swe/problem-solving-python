a94eb9faacfebdafd26ae903d902270eebd414f2
Maruf <marufur1847@gmail.com>
2021-07-28 22:52:22 +0600

    update game time

--- URI/1047_game_time_minutes.py
+++ URI/1047_game_time_minutes.py
@@ -1,22 +1,22 @@
-h1 , m1, h2, m2 = input().split(" ")
+h1, m1, h2, m2 = input().split(" ")
 
-h1=int(h1)
-m1=int(m1)
-h2=int(h2)
-m2=int(m2)
+h1 = int(h1)
+m1 = int(m1)
+h2 = int(h2)
+m2 = int(m2)
 
-rh=h2-h1
+rh = h2 - h1
 
-if rh<0:
-    rh=24+rh
+if rh < 0:
+    rh = 24 + rh
 
-rm=m2-m1
+rm = m2 - m1
 
-if rm<0:
-    rm=60+rm
-    rh-=1
+if rm < 0:
+    rm = 60 + rm
+    rh -= 1
 
-if h1==h2 and m1==m2:
+if h1 == h2 and m1 == m2:
     print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
 else:
-    print("O JOGO DUROU %d HORA(S) E %d MINUTO(S)" %(rh,rm))
+    print("O JOGO DUROU %d HORA(S) E %d MINUTO(S)" % (rh, rm))

de3bf5f669415d3ff2b8b33ff8465815bdb7fd8a
Maruf <marufur1847@gmail.com>
2021-07-28 22:51:11 +0600

    add game time

--- URI/1047_game_time_minutes.py
+++ URI/1047_game_time_minutes.py
@@ -1,14 +1,22 @@
-# s_h = start hour, s_m = start minute, f_h = finish hour, f_m = finish minute
-s_h, s_m, f_h, f_m = list(map(int, input().split()))
-total_hours = f_h - s_h
-if (total_hours < 0):
-    total_hours = 24 + (f_h - s_h)
-total_minutes = f_m - s_m
-if (total_minutes < 0):
-    total_minutes = 60 + (f_m - s_m)
-    total_hours = total_hours - 1
+h1 , m1, h2, m2 = input().split(" ")
 
-if (f_h == s_h and f_m == s_m):
+h1=int(h1)
+m1=int(m1)
+h2=int(h2)
+m2=int(m2)
+
+rh=h2-h1
+
+if rh<0:
+    rh=24+rh
+
+rm=m2-m1
+
+if rm<0:
+    rm=60+rm
+    rh-=1
+
+if h1==h2 and m1==m2:
     print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
 else:
-    print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(total_hours, total_minutes))
+    print("O JOGO DUROU %d HORA(S) E %d MINUTO(S)" %(rh,rm))

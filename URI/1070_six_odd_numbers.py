e6167d419dcc7b8267546c74a9a8004b09f3dea4
Maruf <marufur1847@gmail.com>
2021-07-30 23:11:28 +0600

    add another solution

--- URI/1070_six_odd_numbers.py
+++ URI/1070_six_odd_numbers.py
@@ -8,3 +8,13 @@ while True:
     if count == 6:
         break
 
+"""
+another solution 
+x = int(input())
+i = 0
+while (i < 6):
+    if (x % 2 == 1):
+        print(x)
+        i = i + 1
+    x += 1
+"""

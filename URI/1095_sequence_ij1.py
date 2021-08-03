44eb76932643fabf0d88de6b023a01b76c89566f
Maruf <marufur1847@gmail.com>
2021-08-03 23:53:20 +0600

    1095 another solution add

--- URI/1095_sequence_ij1.py
+++ URI/1095_sequence_ij1.py
@@ -10,7 +10,13 @@ while True:
 # another
 i = 1
 j = 60
-while j >=0:
+while j >= 0:
     print(f'I={i} J={j}')
     i += 3
     j -= 5
+
+# another...using for loop
+i = 1
+for j in range(60, -1, -5):
+    print(f'I={i} J={j}')
+    i += 3

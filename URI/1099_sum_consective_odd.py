f97d0d5d03f39e1c1bf6c3d04f96ff39895c8508
Maruf <marufur1847@gmail.com>
2021-08-04 00:32:08 +0600

    add 1099 odd II another solution

--- URI/1099_sum_consective_odd.py
+++ URI/1099_sum_consective_odd.py
@@ -8,3 +8,21 @@ for i in range(n):
         if j % 2 == 1:
             sum += j
     print(sum)
+
+
+n = int(input())
+for i in range(n):
+    a, b = list(map(int, input().split()))
+    d = 0
+    if (a == b):
+        print(0)
+    else:
+        c = 0
+        if (a > b):
+            c = a
+            a = b
+            b = c
+        for j in range(a + 1, b, 1):
+            if (j % 2 != 0):
+                d += j
+        print(d)
